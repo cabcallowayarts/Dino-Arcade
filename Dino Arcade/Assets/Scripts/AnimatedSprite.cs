db73f338ad82885edc8765f4c4a4ace47e2e3b03
FluffyChipmunk <66707110+FluffyChipmunk@users.noreply.github.com>
2022-01-13 21:42:13 -0500

    dino animated

--- Dino Arcade/Assets/Scripts/AnimatedSprite.cs
+++ Dino Arcade/Assets/Scripts/AnimatedSprite.cs
@@ -7,13 +7,102 @@ using UnityEngine;
 public class AnimatedSprite : MonoBehaviour
 {
     public SpriteRenderer spriteRenderer { get; private set; }
-    public AnimatedSprite[] sprites;
+    public Sprite[] sprites;
     public float animationTime = .25f;
     public int animationFrame { get; private set; }
     public bool loop = true;
+    public Sprite[] verticalSprites;
+    public bool isHorizontal = true;
 
     private void Awake()
     {
         this.spriteRenderer = GetComponent<SpriteRenderer>();
     }
+
+    private void Start()
+    {
+        InvokeRepeating(nameof(WhichRepeat), this.animationTime, this.animationTime);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            this.AdvanceVertical();
+            isHorizontal = false;
+        }
+        else if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            this.AdvanceHorizontal();
+            isHorizontal = true;
+        }
+        else
+        {
+            return;
+        }
+    }
+
+    public void WhichRepeat()
+    {
+        if(isHorizontal)
+        {
+            AdvanceHorizontal();
+        }
+        else
+        {
+            AdvanceVertical();
+        }
+    }
+
+    public void AdvanceHorizontal()
+    {
+        
+
+
+            if (!this.spriteRenderer.enabled)
+            {
+                return;
+            }
+
+            this.animationFrame++;
+            if (this.animationFrame >= this.sprites.Length && this.loop)
+            {
+                this.animationFrame = 0;
+            }
+            if (this.animationFrame >= 0 && this.animationFrame < this.sprites.Length)
+            {
+                this.spriteRenderer.sprite = this.sprites[this.animationFrame];
+            }
+        
+    }
+
+    public void AdvanceVertical()
+    {
+        
+        
+
+        
+        if (!this.spriteRenderer.enabled)
+        {
+            return;
+        }
+
+        this.animationFrame++;
+        if (this.animationFrame >= this.verticalSprites.Length && this.loop)
+        {
+            this.animationFrame = 0;
+        }
+        if (this.animationFrame >= 0 && this.animationFrame < this.verticalSprites.Length)
+        {
+            this.spriteRenderer.sprite = this.verticalSprites[this.animationFrame];
+        }
+        
+    }
+
+    public void Restart()
+    {
+        this.animationFrame = -1;
+        AdvanceHorizontal();
+    }
+
 }
--- Dino Arcade/Assets/Scripts/Pacman.cs
+++ Dino Arcade/Assets/Scripts/Pacman.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Pacman : MonoBehaviour
 {
     public AnimatedSprite deathSequence;
+    //public AnimatedSprite Dino;
     public SpriteRenderer spriteRenderer { get; private set; }
     public new Collider2D collider { get; private set; }
     public Movement movement { get; private set; }
@@ -14,6 +15,7 @@ public class Pacman : MonoBehaviour
         this.spriteRenderer = GetComponent<SpriteRenderer>();
         this.collider = GetComponent<Collider2D>();
         this.movement = GetComponent<Movement>();
+        //this.Dino = 
     }
 
     private void Update()
@@ -22,24 +24,41 @@ public class Pacman : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
         {
             this.movement.SetDirection(Vector2.up);
+            //Dino.AdvanceVertical();
+            this.spriteRenderer.flipY = false;
+            
+
         }
         else if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
         {
             this.movement.SetDirection(Vector2.down);
+            //Dino.AdvanceVertical();
+            this.spriteRenderer.flipY = true;
+            
+
         }
         else if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
         {
             this.movement.SetDirection(Vector2.left);
+            //Dino.AdvanceHorizontal();
+            this.spriteRenderer.flipY = false;
+            this.spriteRenderer.flipX = false;
         }
         else if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
         {
             this.movement.SetDirection(Vector2.right);
+            //Dino.AdvanceHorizontal();
+            this.spriteRenderer.flipY = false;
+            this.spriteRenderer.flipX = true;
         }
 
+
         // Rotate pacman to face the movement direction
-        float angle = Mathf.Atan2(this.movement.direction.y, this.movement.direction.x);
-        this.transform.rotation = Quaternion.AngleAxis(angle * Mathf.Rad2Deg, Vector3.forward);
+        //float angle = Mathf.Atan2(this.movement.direction.y, this.movement.direction.x);
+        //this.transform.rotation = Quaternion.AngleAxis(angle * Mathf.Rad2Deg, Vector3.forward);
     }
+
+    
 
     public void ResetState()
     {
